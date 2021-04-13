f34189271a2f9c00037bc08667372c8ead292253
Vitaliy <difo89nur@gmail.com>
2021-04-13 14:45:47 +0300

    Adding get, post, put, delete mappings

--- src/main/java/com/vitaliykairachka/phone_book/controller/RestApiController.java
+++ src/main/java/com/vitaliykairachka/phone_book/controller/RestApiController.java
@@ -1,13 +1,43 @@
 package com.vitaliykairachka.phone_book.controller;
 
+import com.vitaliykairachka.phone_book.entity.User;
 import com.vitaliykairachka.phone_book.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> showAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable int id) {
+        return userService.getUser(id);
+    }
+
+    @PostMapping("/users")
+    public User addNewUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return user;
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return "User with ID = " + id + " was deleted";
+    }
 }
--- src/main/java/com/vitaliykairachka/phone_book/service/UserServiceImplementation.java
+++ src/main/java/com/vitaliykairachka/phone_book/service/UserServiceImplementation.java
@@ -3,10 +3,12 @@ package com.vitaliykairachka.phone_book.service;
 import com.vitaliykairachka.phone_book.dao.UserRepository;
 import com.vitaliykairachka.phone_book.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserServiceImplementation implements UserService {
     @Autowired
     private UserRepository userRepository;
